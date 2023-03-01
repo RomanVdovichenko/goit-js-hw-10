220641347b7821f46a056e412be87d467f09cf00
Roman Vdovichenko <vrn6870@gmail.com>
2023-03-01 05:28:21 +0200

    new1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
+// var debounce = require('lodash.debounce');
 
 const DEBOUNCE_DELAY = 300;
+console.dir(Notiflix);
+console.dir(debounce);
